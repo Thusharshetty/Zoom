5374f1229dc2e774de170ac60537f63cecb63546
Thushar shetty <shettythushar31@gmail.com>
2026-04-10 12:22:25 +0530

    update1

--- Frontend/src/pages/VideoComponent.jsx
+++ Frontend/src/pages/VideoComponent.jsx
@@ -21,7 +21,9 @@ var connections = {};
 
 const peerConfigConnections = {
     "iceServers": [
-        { "urls": "stun:stun.l.google.com:19302" }
+        { "urls": "stun:stun.l.google.com:19302" },
+        { "urls": "stun:global.stun.twilio.com:3478" },
+        { "urls": "stun:stun.services.mozilla.com" }
     ]
 }
 
